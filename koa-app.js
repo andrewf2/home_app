c961e29beaddfa84cbc3578f9f2851324b63d60b
Andrew Ferguson <andrewf02@gmail.com>
2015-10-04 19:14:47 +0000

    renamed my stupid function name to a better one

--- koa-app.js
+++ koa-app.js
@@ -12,13 +12,13 @@ var Router = require('koa-router');
 var router = new Router();
 
 router.get('/', function *(next) {
-  this.body = yield readFileThunk(__dirname + '/client/app/index.html');
+  this.body = yield readFileFunction(__dirname + '/client/app/index.html');
 });
 
 
 app.use(router.routes());
 
-var readFileThunk = function(src) {
+var readFileFunction = function(src) {
   return new Promise(function (resolve, reject) {
     fs.readFile(src, {'encoding': 'utf8'}, function (err, data) {
       if(err) return reject(err);
